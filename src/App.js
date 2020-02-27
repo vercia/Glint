a5e67e4e1b9956a459a2f44f3b4bb9ebb4f835b1
Weronika <werawera112@gmail.com>
2020-02-27 22:09:12 +0100

    add components to main App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Background from './Components/Background';
+import Nav from './Components/Nav';
+import BackgroundText from './Components/BackgroundText';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <Nav />
       <Background />
+      <BackgroundText />
     </div>
   );
 }
