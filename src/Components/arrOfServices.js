eb091dac6ace9b4375010cf3bf61cb968fec9330
Weronika <werawera112@gmail.com>
2020-03-11 19:22:41 +0100

    move array of services to file

---
+++ src/Components/arrOfServices.js
@@ -0,0 +1,40 @@
+const arrOfServices = [
+    {
+        text:
+            'Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium.Quo delectus est.Maiores voluptas ab sit natus veritatis ut.Debitis nulla cumque veritatis.Sunt suscipit voluptas ipsa in tempora esse soluta sint.',
+        icon: 'fas fa-paint-brush',
+        title: 'Brand Identity'
+    },
+    {
+        text:
+            'Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium.Quo delectus est.Maiores voluptas ab sit natus veritatis ut.Debitis nulla cumque veritatis.Sunt suscipit voluptas ipsa in tempora esse soluta sint.',
+        icon: 'fas fa-object-group',
+        title: 'Illustration'
+    },
+    {
+        text:
+            'Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium.Quo delectus est.Maiores voluptas ab sit natus veritatis ut.Debitis nulla cumque veritatis.Sunt suscipit voluptas ipsa in tempora esse soluta sint.',
+        icon: 'fas fa-bullhorn',
+        title: 'Marketing'
+    },
+    {
+        text:
+            'Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium.Quo delectus est.Maiores voluptas ab sit natus veritatis ut.Debitis nulla cumque veritatis.Sunt suscipit voluptas ipsa in tempora esse soluta sint.',
+        icon: 'fas fa-globe-americas',
+        title: 'Web Design'
+    },
+    {
+        text:
+            'Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium.Quo delectus est.Maiores voluptas ab sit natus veritatis ut.Debitis nulla cumque veritatis.Sunt suscipit voluptas ipsa in tempora esse soluta sint.',
+        icon: 'fas fa-cube',
+        title: 'Packaging Design'
+    },
+    {
+        text:
+            'Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium.Quo delectus est.Maiores voluptas ab sit natus veritatis ut.Debitis nulla cumque veritatis.Sunt suscipit voluptas ipsa in tempora esse soluta sint.',
+        icon: 'fas fa-shapes',
+        title: 'Web Development'
+    }
+];
+
+export default arrOfServices
