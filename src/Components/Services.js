e64544fa76183078718cd42fc86232dfbd4a64b7
Weronika <werawera112@gmail.com>
2020-03-12 17:42:13 +0100

    import arrOfServices

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -3,48 +3,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import Header from './Header';
 import Service from './Service'
 import ScrollAnimation from 'react-animate-on-scroll';
+import arrOfServices from './arrOfServices'
 
 const Services = () => {
   const classes = useStyles();
-
-  const arrOfServices = [
-    {
-      text:
-        'Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium.Quo delectus est.Maiores voluptas ab sit natus veritatis ut.Debitis nulla cumque veritatis.Sunt suscipit voluptas ipsa in tempora esse soluta sint.',
-      icon: 'fas fa-paint-brush',
-      title: 'Brand Identity'
-    },
-    {
-      text:
-        'Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium.Quo delectus est.Maiores voluptas ab sit natus veritatis ut.Debitis nulla cumque veritatis.Sunt suscipit voluptas ipsa in tempora esse soluta sint.',
-      icon: 'fas fa-object-group',
-      title: 'Illustration'
-    },
-    {
-      text:
-        'Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium.Quo delectus est.Maiores voluptas ab sit natus veritatis ut.Debitis nulla cumque veritatis.Sunt suscipit voluptas ipsa in tempora esse soluta sint.',
-      icon: 'fas fa-bullhorn',
-      title: 'Marketing'
-    },
-    {
-      text:
-        'Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium.Quo delectus est.Maiores voluptas ab sit natus veritatis ut.Debitis nulla cumque veritatis.Sunt suscipit voluptas ipsa in tempora esse soluta sint.',
-      icon: 'fas fa-globe-americas',
-      title: 'Web Design'
-    },
-    {
-      text:
-        'Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium.Quo delectus est.Maiores voluptas ab sit natus veritatis ut.Debitis nulla cumque veritatis.Sunt suscipit voluptas ipsa in tempora esse soluta sint.',
-      icon: 'fas fa-cube',
-      title: 'Packaging Design'
-    },
-    {
-      text:
-        'Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium.Quo delectus est.Maiores voluptas ab sit natus veritatis ut.Debitis nulla cumque veritatis.Sunt suscipit voluptas ipsa in tempora esse soluta sint.',
-      icon: 'fas fa-shapes',
-      title: 'Web Development'
-    }
-  ];
 
   return (
     <div className={classes.container} id='services'>
